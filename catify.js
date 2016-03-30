eea0cac3bb848d38071c72a7a8a5fc34f0c1ea9d
Caleigh <caleighchong@gmail.com>
2016-03-30 13:39:10 -0400

    3/30/2016 Update
    Large Update
    
    Added
    373 and 452 projects html and css files
    icons and cat icons for both
    
    Updated
    my picture
    resume
    js file

--- catify.js
+++ catify.js
@@ -3,10 +3,12 @@ function catify() {
 	if(document.getElementById('cat').src.indexOf("catbubble") > -1) {
 		document.getElementById('title').innerHTML="Catleigh Chong";
 		document.getElementById('name').innerHTML="Catleigh Chong";
-		document.getElementById('me').src="./catme.png";
+		document.getElementById('me').src="./catme.jpg";
 		document.getElementById('umich').src="./cat3.png";
 		document.getElementById('cat').src="./elephant.jpg";
 		document.getElementById('pepp').src="./cat5.png";
+		document.getElementById('452').src="./cat8.png";
+		document.getElementById('373').src="./cat7.png";
 		document.getElementById('beagle').src="./cat4.png";
 		document.getElementById('inverter').src="./cat2.png";
 		document.getElementById('shanghai').src="./cat6.png";
@@ -15,10 +17,12 @@ function catify() {
 	else if (document.getElementById('cat').src.indexOf("elephant") > -1) {
 		document.getElementById('title').innerHTML="Caleigh Chong";
 		document.getElementById('name').innerHTML="Caleigh Chong";
-		document.getElementById('me').src="./me.png";
+		document.getElementById('me').src="./me.jpg";
 		document.getElementById('umich').src="./michigan.png";
 		document.getElementById('cat').src="./catbubble.png";
 		document.getElementById('pepp').src="./pepp.jpg";
+		document.getElementById('452').src="./intersectmat.jpg";
+		document.getElementById('373').src="./tankgame.jpg";
 		document.getElementById('beagle').src="./beaglebone.jpg";
 		document.getElementById('inverter').src="./inverter.png";
 		document.getElementById('shanghai').src="./shanghai.jpg";
